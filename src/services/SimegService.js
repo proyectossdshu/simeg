f3ec131a79623556e9ae6bd148949a7bfc3068c5
Eduardo Borraz <jborrazj@guanajuato.gob.mx>
2022-12-20 13:32:45 -0600

    add: se omite api

--- src/services/SimegService.js
+++ src/services/SimegService.js
@@ -16,12 +16,12 @@ class SimegService {
   };
 
   ///Eliminar api
-  getEvaluatedPrograms = (params) => {
+  /* getEvaluatedPrograms = (params) => {
     return ApiExec(params, "simeg/get-evaluated-programs", "POST")
       .then((res) => res)
       .then((res) => res)
       .catch((e) => e);
-  };
+  }; */
 }
 
 export default new SimegService();
